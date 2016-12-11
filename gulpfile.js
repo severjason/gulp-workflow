15d1c261bcf13a2d39f47d4afc498e24c947a9fb
severjason <ssever2010@gmail.com>
2016-12-11 19:17:05 +0200

    Added typescript support

--- gulpfile.js
+++ gulpfile.js
@@ -1,133 +1,144 @@
+(function () {
 
-const gulp = require('gulp'),
-    browserSync = require('browser-sync'),
-    del = require('del'),
-    runSequence = require('run-sequence'),
-    $ = require('gulp-load-plugins')();
+    const gulp = require('gulp'),
+        browserSync = require('browser-sync'),
+        del = require('del'),
+        babel = require('gulp-babel'),
+        cache = require('gulp-cache'),
+        gulpIf = require('gulp-if'),
+        sass = require('gulp-sass'),
+        useref = require('gulp-useref'),
+        uglify = require('gulp-uglify'),
+        cssNano = require('gulp-cssnano'),
+        imageMin = require('gulp-imagemin'),
+        sourceMaps = require('gulp-sourcemaps'),
+        runSequence = require('run-sequence'),
+        ts = require("gulp-typescript"),
+        tsProject = ts.createProject("tsconfig.json");
 
-/**
- *  All paths
- */
-const path = {
-    app: 'app',
-    scss: 'app/scss/**/*.scss',
-    images: 'app/images/**/*.*+(png|jpeg|jpg|svg|gif)',
-    fonts: ['bower_components/bootstrap-sass/assets/fonts/**/*'],
-    libJs: [
-        /*'bower_components/fabric.js/dist/fabric.min.js',
-         'bower_components/jquery/dist/jquery.min.js',
-         'bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js'*/
-    ],
-    js: ['app/js/**/*.js']
-};
+    /**
+     *  All paths
+     */
+    const path = {
+        app: 'app/',
+        scss: 'app/scss/**/*.scss',
+        images: 'app/images/**/*.*+(png|jpeg|jpg|svg|gif)',
+        fonts: ['bower_components/bootstrap-sass/assets/fonts/**/*'],
+        ts: ['app/ts/**/*.ts'],
+        tsCompiled: 'ts/compiled/',
+        js: ['app/js/**/*.js']
+    };
 
-/**
- *  Browser sync
- */
-gulp.task('browserSync', function () {
-    browserSync({
-        server: {
-            baseDir: path.app + "/",
-            routes: {
-                '/bower_components': 'bower_components'
+    /**
+     *  Browser sync
+     */
+    gulp.task('browserSync', function () {
+        browserSync({
+            server: {
+                baseDir: path.app,
+                routes: {
+                    '/bower_components': 'bower_components'
+                }
             }
-        }
+        });
     });
-});
 
-/**
- *  All paths
- */
-gulp.task('css', function () {
-    return gulp.src(path.scss)
-        .pipe($.sourcemaps.init())
-        //.pipe($.sass({outputStyle: 'expanded'}))
-        .pipe($.sass({outputStyle: 'compressed'}))
-        .on('error', $.sass.logError)
-        .pipe($.sourcemaps.write('.'))
-        .pipe(gulp.dest(path.app + '/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
-});
+    /**
+     *  All paths
+     */
+    gulp.task('css', function () {
+        return gulp.src(path.scss)
+            .pipe(sourceMaps.init())
+            //.pipe($.sass({outputStyle: 'expanded'}))
+            .pipe(sass({outputStyle: 'compressed'}))
+            .on('error', sass.logError)
+            .pipe(sourceMaps.write('.'))
+            .pipe(gulp.dest(path.app + 'css'))
+            .pipe(browserSync.reload({
+                stream: true
+            }));
+    });
 
-/**
- *  Move all bower js files to app/lib
- */
-gulp.task('js', function () {
-    return gulp.src(path.libJs)
-        .pipe(gulp.dest('dist/lib'));
-});
+    /**
+     * Move compiled ts to compiled dir
+     */
+    gulp.task('ts', function () {
+        return tsProject.src()
+            .pipe(tsProject())
+            .js.pipe(gulp.dest(path.app + path.tsCompiled));
+    });
 
-/**
- *  Concat all js and css into one file
- */
-gulp.task('useref', function () {
-    return gulp.src(path.app + '/**/*.html')
-        .pipe($.useref())
-        .pipe($.if('*.js', $.babel({
-            presets: ['es2015']
-        })))
-        .pipe($.if('*.js', $.uglify()))
-        .pipe($.if('*.css', $.cssnano()))
-        .pipe(gulp.dest('dist'));
-});
+    /**
+     *  Concat all js and css into one file
+     */
+    gulp.task('useref', function () {
+        return gulp.src(path.app + '**/*.html')
+            .pipe(useref())
+            .pipe(gulpIf(['*.js', path.tsCompiled + "*.js"], babel({
+                presets: ['es2015']
+            })))
+            .pipe(gulpIf(['*.js', path.tsCompiled + "*.js"], uglify()))
+            .pipe(gulpIf('*.css', cssNano()))
+            .pipe(gulp.dest('dist'));
+    });
 
-/**
- *  Move all images to dist
- */
-gulp.task('img', function () {
-    return gulp.src(path.images)
-        .pipe($.cache($.imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: false
-        })))
-        .pipe(gulp.dest('dist/images'))
-});
+    /**
+     *  Move all images to dist
+     */
+    gulp.task('img', function () {
+        return gulp.src(path.images)
+            .pipe(cache(imageMin({
+                optimizationLevel: 3,
+                progressive: true,
+                interlaced: false
+            })))
+            .pipe(gulp.dest('dist/images'))
+    });
 
-/**
- *  AMove all fonts to dist and app
- */
-gulp.task('fonts', function () {
-    return gulp.src(path.fonts)
-        .pipe(gulp.dest(path.app + '/fonts'))
-        .pipe(gulp.dest('dist/fonts'));
-});
+    /**
+     *  AMove all fonts to dist and app
+     */
+    gulp.task('fonts', function () {
+        return gulp.src(path.fonts)
+            .pipe(gulp.dest(path.app + 'fonts'))
+            .pipe(gulp.dest('dist/fonts'));
+    });
 
-/**
- *  Delete dist folder
- */
-gulp.task('clean', function (callback) {
-    del('dist');
-    return $.cache.clearAll(callback);
-});
+    /**
+     *  Delete dist folder
+     */
+    gulp.task('clear', function (callback) {
+        del('dist');
+        return cache.clearAll(callback);
+    });
 
-/**
- *  Delete all in dist folder except images
- */
-gulp.task('clean:dist', function (callback) {
-    del(['dist/**/*', '!dist/images', '!dist/images/**/*'], callback)
-});
+    /**
+     *  Delete all in dist folder except images
+     */
+    gulp.task('clear:dist', function (callback) {
+        del(['dist/**/*', '!dist/images', '!dist/images/**/*'], callback)
+    });
 
-/**
- *  Watch task - for sass
- */
-gulp.task('watch', ['browserSync', 'css'], function () {
-    gulp.watch(path.scss, ['css']);
-    gulp.watch(["app/*.html","app/scss/*.scss"]).on('change', browserSync.reload);
-});
+    /**
+     *  Watch task - for sass
+     */
+    gulp.task('watch', ['browserSync', 'css'], function () {
+        gulp.watch(path.ts, ['ts']);
+        gulp.watch(path.scss, ['css']);
+        gulp.watch([path.app + "*.html", path.scss, path.app + path.tsCompiled + "**/*.js"]).on('change', browserSync.reload);
+    });
 
-/**
- *  Task to builds an app for production
- */
-gulp.task('build', function () {
-    runSequence(['css', 'useref','js', 'img', 'fonts'])
-});
+    /**
+     *  Task to builds an app for production
+     */
+    gulp.task('build', function () {
+        runSequence(['css','ts', 'useref', 'img', 'fonts'])
+    });
 
-/**
- *  Default task
- */
-gulp.task('default', function () {
-    runSequence(['css', 'js', 'fonts', 'browserSync', 'watch'])
-});
+    /**
+     *  Default task
+     */
+    gulp.task('default', function () {
+        runSequence(['css', 'fonts', 'browserSync', 'watch'])
+    });
+}());
